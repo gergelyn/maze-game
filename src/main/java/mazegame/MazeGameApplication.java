cbf3e207f55bf76fd8b8d559db52d49d16ede1cd
gergelyn <preszthegas@gmail.com>
2021-05-13 23:19:27 +0200

    board.fxml created with Scene Builder, board.css created for board.fxml and MazeGameApplication initialized

--- src/main/java/mazegame/MazeGameApplication.java
+++ src/main/java/mazegame/MazeGameApplication.java
@@ -1,6 +1,21 @@
 package mazegame;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MazeGameApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/board.fxml"));
+        stage.setTitle("Maze Game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
